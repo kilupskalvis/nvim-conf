0a32de7d4bbf55894cfe3284be5511bd779d4628
kilupskalvis <kilupsk@gmail.com>
2026-03-04 12:24:41 +0100

    fix: clean up diffview orphaned buffers on close

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,5 @@
+local pre_bufs = {}
+
 return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
@@ -7,6 +9,29 @@ return {
   },
   opts = {
     watch_index = true,
+    hooks = {
+      view_opened = function()
+        pre_bufs = {}
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
+            pre_bufs[buf] = true
+          end
+        end
+      end,
+      view_closed = function()
+        vim.defer_fn(function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_is_valid(buf) and not pre_bufs[buf] then
+              local name = vim.api.nvim_buf_get_name(buf)
+              if name ~= "" then
+                vim.api.nvim_buf_delete(buf, { force = true })
+              end
+            end
+          end
+          pre_bufs = {}
+        end, 100)
+      end,
+    },
     keymaps = {
       view = {
         { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
