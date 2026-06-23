2983fb3f2456763878e42539462bb0807fb1c3d2
kilupskalvis <kilupsk@gmail.com>
2026-06-23 10:17:41 +0200

    fix: make q close tab after gf regardless of buffer

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,25 @@
 local state = { active = false, pre_bufs = {} }
 
+local function goto_file_in_tab()
+  require("diffview.actions").goto_file_tab()
+  local tab = vim.api.nvim_get_current_tabpage()
+  local group = vim.api.nvim_create_augroup("DiffviewGfTab" .. tab, { clear = true })
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    callback = function()
+      if vim.api.nvim_get_current_tabpage() ~= tab then return end
+      vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = true, desc = "Close tab" })
+    end,
+  })
+  vim.api.nvim_create_autocmd("TabClosed", {
+    group = group,
+    callback = function()
+      pcall(vim.api.nvim_del_augroup_by_id, group)
+    end,
+  })
+  vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = true, desc = "Close tab" })
+end
+
 return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
@@ -48,15 +68,7 @@ return {
     keymaps = {
       view = {
         { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
-        {
-          "n",
-          "gf",
-          function()
-            require("diffview.actions").goto_file_tab()
-            vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = true, desc = "Back to Diffview" })
-          end,
-          { desc = "Open file in new tab" },
-        },
+        { "n", "gf", goto_file_in_tab, { desc = "Open file in new tab" } },
       },
       file_panel = {
         { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
@@ -103,27 +115,11 @@ return {
           end,
           { desc = "Discard file/directory changes" },
         },
-        {
-          "n",
-          "gf",
-          function()
-            require("diffview.actions").goto_file_tab()
-            vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = true, desc = "Back to Diffview" })
-          end,
-          { desc = "Open file in new tab" },
-        },
+        { "n", "gf", goto_file_in_tab, { desc = "Open file in new tab" } },
       },
       file_history_panel = {
         { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
-        {
-          "n",
-          "gf",
-          function()
-            require("diffview.actions").goto_file_tab()
-            vim.keymap.set("n", "q", "<cmd>tabclose<cr>", { buffer = true, desc = "Back to Diffview" })
-          end,
-          { desc = "Open file in new tab" },
-        },
+        { "n", "gf", goto_file_in_tab, { desc = "Open file in new tab" } },
       },
     },
   },
